274e7ad97db9c87f4402df39291d9c625ce55a7f
dowhep <caogangshen@gmail.com>
2020-02-11 19:19:45 -0800

    update framework to load and save data

---
+++ NRGScoutingApp2020/Algorithms/CacheData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NRGScoutingApp2020.Algorithms
+{
+    public class CacheData
+    {
+        /// <summary> This method create cache files
+        /// for the events in memory to save all of them
+        /// </summary>
+        /// <param name="matchEvents"></param>
+        public void CacheEvents(List<MatchEventClass> matchEvents)
+        {
+            
+        }
+    }
+}
---
+++ NRGScoutingApp2020/Algorithms/LoadData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NRGScoutingApp2020.Algorithms
+{
+    public class LoadData
+    {
+        /// <summary> This method load the competitions
+        /// from cache to memory for the app
+        /// </summary>
+        public void LoadEvents()
+        {
+
+        }
+    }
+}
