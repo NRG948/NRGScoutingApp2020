40abc92d03493b20a97f6afc1422e3159e0ea97b
dowhep <caogangshen@gmail.com>
2020-02-11 19:01:17 -0800

    create data objects and structure

---
+++ NRGScoutingApp2020/Algorithms/Converter.cs
@@ -0,0 +1,57 @@
+﻿using NRGScoutingApp2020.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NRGScoutingApp2020.Algorithms
+{
+    public class Converter
+    {
+        /*
+         * Convert a matchevent object to a JSON object
+         * 
+         * pre: a MatchEventClass object
+         * post: a JSON string
+         */
+        public string eventToJSON()
+        {
+            return "";
+        }
+        
+        /*
+         * Convert a string of JSON object to a matchevent object
+         * 
+         * pre: a JSON string 
+         * post: a MatchEventClass object
+         */
+
+        public MatchEventClass JSONToEvent()
+        {
+            return new MatchEventClass();
+        }
+
+        /*
+         * Convert a match object to a JSON object
+         * 
+         * pre: a Match object
+         * post: a JSON string
+         */
+        public string matchToJSON()
+        {
+            return "";
+        }
+
+        /*
+         * Convert a string of JSON object to a match object
+         * 
+         * pre: a JSON string 
+         * post: a Match object
+         */
+
+        public Match JSONToMatch()
+        {
+            return new Match();
+        }
+
+    }
+}
---
+++ NRGScoutingApp2020/Data/Match.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NRGScoutingApp2020.Data
+{
+    public class Match
+    {
+
+        public int number = 0;
+        public Match()
+        {
+
+        }
+    }
+}
