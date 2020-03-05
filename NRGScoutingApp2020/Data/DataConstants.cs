0551582246591ccb2256e8989da2a9516a6b15ec
dowhep <caogangshen@gmail.com>
2020-03-04 20:55:43 -0800

    Update UI

--- NRGScoutingApp2020/Data/DataConstants.cs
+++ NRGScoutingApp2020/Data/DataConstants.cs
@@ -24,7 +24,7 @@ namespace NRGScoutingApp2020
         public static readonly string canCreate = "create";
         public static readonly string scouted = "scouted";
         public static readonly string main = "main";
-        public static readonly string parameter = "parameter";
+        public static readonly string parameter = "parameters";
         public static readonly string events = "events";
 
         // Prompt Strings
@@ -41,13 +41,29 @@ namespace NRGScoutingApp2020
 
         public static readonly string[] alliancePosition = { "Blue 1", "Blue 2", "Blue 3", "Red 1", "Red 2", "Red 3" };
 
-        public static readonly string[] rankPickerText = { "accuracy" , "lower", "outer", "inner", "climb" };
+        public static readonly string[] rankPickerText = { "Accuracy" , "Lower", "Outer", "Inner", "Climb" };
 
         public static readonly string[] actionTypeList = { "Pick", "Lower", "Outer", "Inner", "Miss"};
 
         public static readonly int[] typeToBallChanged = { 1, -1, -1, -1, -1 };
 
         public static readonly string[] climbOptions = { "No Climb", "Park", "Climb", "Climb and Level" };
+
         public static readonly string[] penaltyOptions = { "No Card", "Yellow Card", "Red Card" };
+
+        public static readonly string[] QUESTIONS = {
+            "What drive base do you use?",
+            "How much does your robot weigh?",
+            "Drive practice hours? (estimate)",
+            "Can you climb? if so where on the bar and at what level can you climb?",
+            "Low goal or high goal?",
+            "Inner port capability?",
+            "Trench run capability?",
+            "How many power cells per match? (estimate)",
+            "What does auto do?",
+            "Anything else you would like to tell us?",
+            "Other?"
+        };
+
     }
 }
